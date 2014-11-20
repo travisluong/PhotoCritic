478fcfcd8a6b737ce05ec6d88396bee8c55df371
Travis Luong <travis@travisluong.com>
2014-11-19 16:28:30 -0800

    style tweaks

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,7 +1,11 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes :id, :title, :image, :thumb
+  attributes :id, :title, :image, :thumb, :medium
 
   def thumb
     object.image.url(:thumb)
+  end
+
+  def medium
+    object.image.url(:medium)
   end
 end
