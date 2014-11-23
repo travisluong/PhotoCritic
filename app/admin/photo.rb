dea6ceb2757b060b2ecaa6be43d79869f7a61fca
Travis Luong <travis@travisluong.com>
2014-11-23 13:38:32 -0800

    user and photo admin

---
+++ app/admin/photo.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register Photo do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+  index as: :grid do |photo|
+    link_to image_tag(photo.pic_url(:thumbnail)), admin_photo_path(photo)
+  end
+
+end
